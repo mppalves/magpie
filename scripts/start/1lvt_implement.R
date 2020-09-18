1a117c72a99357670b7215c6900bfb6f4f8e2502
mppalves <mppalves@gmail.com>
2020-09-18 10:43:42 +0200

    test 32

--- scripts/start/1lvt_implement.R
+++ scripts/start/1lvt_implement.R
@@ -19,7 +19,7 @@ test <- c(1)
 
 
 for(i in 1:length(test)){
-  cfg$title <- paste0("try31_",test[i])
+  cfg$title <- paste0("try32_",test[i])
   cfg$gms$s14_corr_fact <- as.character(test[i])
   cfg$output <- c("rds_report","validation","disaggregation","lsu_evaluation")
   cfg$gms$livestock <- "lvtk_aug20"                  # def = fbask_jan16, lvtk_aug20
