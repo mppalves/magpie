e6761b913d546a2be2e8241c61a8831c35ebf950
mppalves <mppalves@gmail.com>
2020-10-02 15:47:15 +0200

    moq 19.3

--- scripts/start/start_livestock.R
+++ scripts/start/start_livestock.R
@@ -20,7 +20,7 @@ test <- c(1)
 
 
 for(i in 1:length(test)){
-  cfg$title <- paste0("mow19.2_",test[i])
+  cfg$title <- paste0("mow19.3_",test[i])
   cfg$gms$s70_dist_fact <- as.character(test[i])
   cfg$output <- c("rds_report","lsu_evaluation","validation","disaggregation")
   cfg$gms$livestock <- "lvtk_aug20"           # def = fbask_jan16, lvtk_aug20
