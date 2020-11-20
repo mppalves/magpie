d90abc8d5f79005eae03c10ef2f228e47c1b77da
mppalves <mppalves@gmail.com>
2020-11-20 13:43:49 +0100

    2pmgmt_hdgem2_8p5_cc_3

--- scripts/start/start_livestock.R
+++ scripts/start/start_livestock.R
@@ -27,8 +27,8 @@ for(i in 1:length(test)){
            "calibration_H12_c200_26Feb20.tgz",
            "additional_data_rev3.86.tgz")
   cfg$force_download <- FALSE
-  #cfg$title <- paste0("2pmgmt_hdgem2_8p5_cc_3")
-  #cfg$output <- c("rds_report","lsu_evaluation","validation","disaggregation")
+  cfg$title <- paste0("2pmgmt_hdgem2_8p5_cc_3")
+  cfg$output <- c("rds_report","lsu_evaluation","validation","disaggregation")
   cfg$gms$livestock <- "lvtk_aug20"           # def = fbask_jan16, lvtk_aug20
   cfg$gms$yields <- "dynamic_aug20"           # def = dynamic_aug18,dynamic_aug20
   cfg$gms$past <- "develop_set20"             # def = endo_jun13
@@ -39,15 +39,15 @@ for(i in 1:length(test)){
   cfg$gms$s31_fac_req_past  <- as.character(test[i])
 #  cfg$gms$trade <- "free_apr16"             # def = selfsuff_reduced
 #  cfg$recalibrate <- "TRUE"
-#  start_run(cfg=cfg,codeCheck=F)
+  start_run(cfg=cfg,codeCheck=F)
 
-  cfg$results_folder <- "output/:title:"
-  cfg$recalibrate <- TRUE
-  cfg$title <- "calib_run"
-  cfg$gms$c_timesteps <- 1
-  cfg$output <- c("report")
-  cfg$sequential <- TRUE
-  start_run(cfg,codeCheck=FALSE)
-  magpie4::submitCalibration("H12")
+#  cfg$results_folder <- "output/:title:"
+#  cfg$recalibrate <- TRUE
+#  cfg$title <- "calib_run"
+#  cfg$gms$c_timesteps <- 1
+#  cfg$output <- c("report")
+#  cfg$sequential <- TRUE
+#  start_run(cfg,codeCheck=FALSE)
+#  magpie4::submitCalibration("H12")
 
 }
