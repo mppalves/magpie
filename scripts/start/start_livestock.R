6fb9fc8a4b562100f4c10175cbf8ab308404f70f
mppalves <mppalves@gmail.com>
2020-11-22 14:57:46 +0100

    experiment_3

--- scripts/start/start_livestock.R
+++ scripts/start/start_livestock.R
@@ -27,7 +27,7 @@ for(i in 1:length(test)){
            "calibration_H12_c200_26Feb20.tgz",
            "additional_data_rev3.86.tgz")
   cfg$force_download <- FALSE
-  cfg$title <- paste0("experiment_2")
+  cfg$title <- paste0("experiment_3")
   cfg$output <- c("rds_report","lsu_evaluation","validation","disaggregation")
 
   cfg$gms$livestock <- "lvtk_aug20"           # def = fbask_jan16, lvtk_aug20
