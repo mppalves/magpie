894d8e4b5bf1e2f33bc0041056deec9ce68c38d8
mppalves <mppalves@gmail.com>
2020-11-27 13:33:17 +0100

    experiment_20

--- scripts/start/start_livestock.R
+++ scripts/start/start_livestock.R
@@ -28,13 +28,13 @@ for(i in 1:length(test)){
            "additional_data_rev3.86.tgz")
   cfg$force_download <- FALSE
   #cfg$title <- paste0("experiment_11_mod_lpjm_", gsub("\\.","_",test[i]))
-  cfg$title <- paste0("experiment_19")
+  cfg$title <- paste0("experiment_20")
   cfg$output <- c("rds_report","lsu_evaluation","validation","disaggregation")
   #cfg$gms$s31_fac_req_past  <- test[i]
   cfg$gms$livestock <- "lvtk_i_aug20"           # def = fbask_jan16, lvtk_aug20
   cfg$gms$yields <- "nn_aug20"           # def = dynamic_aug18,dynamic_aug20
   cfg$gms$past <- "develop_nov20"             # def = endo_jun13
-  cfg$gms$crop <- "endo_jun13"           # "coup2100"
+  cfg$gms$crop <- "develop_nov20"           # "coup2100"
   cfg$gms$c_timesteps <- "coup2100"           # "coup2100"
   cfg$gms$c14_yields_scenario  <- "nocc"   # def = "nocc"
 
