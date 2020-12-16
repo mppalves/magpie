ce51bccb884854c190bd534a22fc4cd36f824091
mppalves <mppalves@gmail.com>
2020-12-16 15:59:13 +0100

    soil_experiment_17

--- scripts/start/start_livestock.R
+++ scripts/start/start_livestock.R
@@ -28,7 +28,7 @@ for(i in 1:length(test)){
            "additional_data_rev3.86.tgz")
   cfg$force_download <- FALSE
   #cfg$title <- paste0("soil_exp_10_mow_cost_", gsub("\\.","_",test[i]))
-  cfg$title <- paste0("soil_experiment_16")
+  cfg$title <- paste0("soil_experiment_17")
   cfg$output <- c("rds_report","lsu_evaluation","validation","disaggregation")
   cfg$gms$s31_test_scalar <- test[i]
   cfg$gms$livestock <- "lvtk_k_aug20"           # def = fbask_jan16, lvtk_aug20
