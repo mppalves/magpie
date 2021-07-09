20acdab81b19e81a777918686f47b832cf44591a
Marcos Alves <mppalves@gmail.com>
2021-07-09 15:44:35 +0200

    new inputs

--- scripts/output/extra/grassland_soilcarbon.R
+++ scripts/output/extra/grassland_soilcarbon.R
@@ -27,14 +27,26 @@ if (!exists("source_include")) {
 
 land_hr_out_file           <- read.magpie(file.path(outputdir, "cell.land_0.5.mz"))
 lsu_ha_file                <- read.magpie(file.path(outputdir, "lsu_ha.mz"))
-environment_data           <- read.magpie(file.path("./modules/31_past/input/environment_scaled_gramnt.mz"))
-weights                    <- readRDS(Sys.glob(file.path(outputdir, "weights_*.rds")))
-input_names                <- readRDS(Sys.glob(file.path(outputdir, "inputs_*.Rds")))
+environment_data           <- read.magpie(file.path("./modules/31_past/input/environment_gramnt.mz"))
+weights                    <- readRDS(file.path("./modules/31_past/input/", "f31_weights.rds"))
+input_names                <- as.vector(readRDS(file.path("./modules/31_past/input/", "f31_inputs.rds")))
+means_col                  <- readRDS(file.path("./modules/31_past/input/", "f31_mean_col.rds"))
+std_col                    <- readRDS(file.path("./modules/31_past/input/", "f31_stddevs_col.rds"))
+mean_lab                   <- readRDS(file.path("./modules/31_past/input/", "f31_mean_lab.rds"))
+std_lab                    <- readRDS(file.path("./modules/31_past/input/", "f31_stddevs_lab.rds"))
+
+# environment_data           <- read.magpie(file.path("/p/projects/landuse/users/pedrosa/MAgPIE/magpie_2/modules/31_past/input/environment_gramnt.mz"))
+# weights                    <- readRDS(file.path("/p/projects/landuse/users/pedrosa/MAgPIE/magpie_2/modules/31_past/input/", "f31_weights.rds"))
+# input_names                <- as.vector(readRDS(file.path("/p/projects/landuse/users/pedrosa/MAgPIE/magpie_2/modules/31_past/input/", "f31_inputs.rds")))
+# means                      <- readRDS(file.path("/p/projects/landuse/users/pedrosa/MAgPIE/magpie_2/modules/31_past/input/", "f31_mean.rds"))
+# std                        <- readRDS(file.path("/p/projects/landuse/users/pedrosa/MAgPIE/magpie_2/modules/31_past/input/", "f31_std.rds"))
+
 
 soil_carbon_base_file      <- file.path(outputdir,"soil_carbon_base.mz")
 soil_pastr_file            <- file.path(outputdir,"soil_pastr.mz")
 
 load(paste0(outputdir, "/config.Rdata"))
+
 
 ################################################################################
 
@@ -60,9 +72,10 @@ lsu_ha_file <- lsu_ha_file[, years, ]
 input <- mbind(setNames(lsu_ha_file[, , "range"], grep("lsu", input_names, value = T)), environment_data)
 input <- as.data.frame(input)
 input_df <- pivot_wider(input, names_from = Data1, values_from = Value)
-
+input_df_scaled <- scale(input_df[, input_names], center = means_col[input_names], scale = std_col[input_names])
 # Making predictions and saving files
-soil_carbon <- toolNeuralNet(input_df[, input_names], weights, "softplus")
+soil_carbon <- toolNeuralNet(input_df_scaled, weights, "softplus")
+soil_carbon <- scale(soil_carbon, center = mean_lab, scale = std_lab)
 soil_carbon <- cbind(input_df[, c("Cell", "Year")], soil_carbon)
 soil_carbon <- as.magpie(soil_carbon, spatial = 1)
 soil_carbon <- toolCell2isoCell(soil_carbon)
@@ -79,10 +92,3 @@ soil_pastr <- land_hr_out_file[, years, "range"] * soil_carbon
 
 .tmpwrite(soil_pastr, soil_pastr_file, comment = "unit: grams of Carbon per squared meter (gCm)",
   message = "Write outputs soil_pastr \n")
-
-
-#####################################################################################################
-#####################################################################################################
-# Write read anf cal function for the model weights out from the grassland emulator source folder!
-#####################################################################################################
-#####################################################################################################
